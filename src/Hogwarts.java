0cf251c8cc3351f61815f7532a88dcfcb3255775
Eugen Lange <lange.e@mail.ru>
2022-09-13 00:17:23 +0500

    перерыв 1

---
+++ src/Hogwarts.java
@@ -0,0 +1,22 @@
+public class Hogwarts {
+    private int magicPower;
+    private int trangressionAbility;
+    private int nobleness;
+    private int honour;
+    private int bravery;
+    private int hardworkness;
+    private int loyalty;
+    private int sincerity;
+    private int intelligence;
+    private int wisdom;
+    private int wit;
+    private int creativity;
+    private int trickery;
+    private int resoluteness;
+    private int ambition;
+    private int inventiveness;
+    private int lustForPower;
+
+
+}
+
